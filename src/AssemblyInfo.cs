6292196e9164a81616d2c7dd901b9a28d439aadd
Jack Buehner <jackb@furman.edu>
2025-06-05 18:25:19 -0400

    build: add assemblyinfo and exe signing

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Remote Desktop Protocol Handler")]
+[assembly: AssemblyProduct("RDP Protocol Handler")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Jack Buehner")]
+[assembly: AssemblyCopyright("Jack Buehner 2025")]
+[assembly: AssemblyDescription("A protocol handler for rdp://")]
